d29362abcd46c6a586b552472e2e6e033312c4e2
Jay <lemak91@mac.com>
2022-12-02 11:23:02 -0500

    complete

--- main.js
+++ main.js
@@ -13,10 +13,39 @@ const rl = readline.createInterface({
 
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
+
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  }
+
+  else if (hand1 === 'rock' && hand2 === 'scissors') {
+    return "Hand one wins!";
+  }
+
+  else if (hand1 === 'rock' && hand2 === 'paper') {
+    return "Hand two wins!";
+  }
+
+  else if (hand1 === 'paper' && hand2 === 'scissors') {
+    return "Hand two wins!";
+  }
+
+  else if (hand1 === 'paper' && hand2 === 'rock') {
+    return "Hand one wins!";
+  }
+
+  else if (hand1 === 'scissors' && hand2 === 'rock') {
+    return "Hand two wins!";
+  }
+
+  else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!";
+  }
 
   // Write code here
   // Use the unit test to see what is expected
-
 }
 
 // the first function called in the program to get an input from the user
